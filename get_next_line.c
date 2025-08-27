7cd7db56f7dad0ac82d0f7ee63ab031b57b7e45c
Rafaelly Diniz <138537427+devrafaelly@users.noreply.github.com>
2025-08-27 21:24:10 +0000

    feat: updating function

--- get_next_line.c
+++ get_next_line.c
@@ -14,23 +14,67 @@
 
 char	*get_next_line(int fd)
 {
-    char char_read[BUFFER_SIZE];
+    char    char_read[BUFFER_SIZE];
     int bytes_read;
     
     fd = open("bittersuite.txt", O_RDONLY);
     if (fd < 0)
         return (-1);
-    bytes_read = read(BUFFER_SIZE, char_read);
+    while (bytes_read != 0)
+    {
+        bytes_read = read(fd, BUFFER_SIZE, char_read);
+    }
     close(fd);
 }
-
-char    *armazenar_caracteres(char *char_read)
+// como mallocar esse rest? deixar toda a memoria mallocada com \0
+char    *armazenamento_retorno(char *char_read)
 {
+    static char    *rest;
+    char    *line;
     int i;
+    int j;
     
     i = 0;
-    while (char_read[i] || char_read != '\n')
+    while (*rest)
     {
-        
+        if (*rest == '\n')
+        {
+            line = malloc(i);
+            if(!line)
+                return (NULL);
+            i = 0;
+            while (rest[i] != '\n')
+            {
+                line[j] = rest[i];
+            }
+            line[j] = '\n';
+            return (line);
+        }
+        rest++;
+        i++;
     }
+    rest = data_clean(rest, j);
+    j = 0;
+    while (char_read[j])
+    {
+        rest[i] = char_read[j];
+        i++;
+        j++;
+    }
+}
+
+char    *data_clean(char *rest, int i)
+{
+    int j;
+    int end;
+
+    end = i;
+    j = 0;
+    while (*rest)
+    {
+        rest[j] = rest [i];
+        j++;
+        i++;
+    }
+    return (rest);
 }
