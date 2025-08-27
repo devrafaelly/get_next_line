ddc95343a3621faeaeef0143ab5507a081d5aa31
Rafaelly Diniz <138537427+devrafaelly@users.noreply.github.com>
2025-08-27 20:09:11 +0000

    feat: adicional functions

--- get_next_line.c
+++ get_next_line.c
@@ -1,4 +1,36 @@
-char	*get_next_line(int fd);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/27 16:44:10 by codespace         #+#    #+#             */
+/*   Updated: 2025/08/27 18:13:02 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
 {
+    char char_read[BUFFER_SIZE];
+    int bytes_read;
+    
+    fd = open("bittersuite.txt", O_RDONLY);
+    if (fd < 0)
+        return (-1);
+    bytes_read = read(BUFFER_SIZE, char_read);
+    close(fd);
 }
-a
+
+char    *armazenar_caracteres(char *char_read)
+{
+    int i;
+    
+    i = 0;
+    while (char_read[i] || char_read != '\n')
+    {
+        
+    }
+}
--- get_next_line.h
+++ get_next_line.h
@@ -1,6 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/27 16:44:08 by codespace         #+#    #+#             */
+/*   Updated: 2025/08/27 17:34:45 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
 
 # include <stdlib.h>
+# include <fcntl.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1024
+#endif
 
 #endif
